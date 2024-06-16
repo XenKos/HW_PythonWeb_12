48e1f2fb72a705723e42c2a5e53e99b0314967a4
XenKos <158058743+XenKos@users.noreply.github.com>
2024-06-16 12:25:21 +0200

    Auth.py

--- auth.py
+++ auth.py
@@ -4,10 +4,9 @@ from fastapi import HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
-from . import models, crud, database
+from . import crud, database
 from .config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_DAYS
-from .schemas import TokenData
-
+from .schemas import TokenData, UserCreate
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -39,14 +38,23 @@ def verify_token(token: str, credentials_exception):
         raise credentials_exception
     return token_data
 
+def register_user(user: UserCreate, db: Session):
+    existing_user = crud.get_user_by_email(db, email=user.email)
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Електронна адреса вже зареєстрована")
+    
+    new_user = crud.create_user(db=db, user=user)
+    
+    return new_user
+
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
+        detail="Не вдалося підтвердити авторизацію",
         headers={"WWW-Authenticate": "Bearer"},
     )
     payload = verify_token(token, credentials_exception)
-    user = crud.get_user_by_email(db, email=payload.get("sub"))
+    user = crud.get_user_by_email(db, email=payload.email)
     if user is None:
         raise credentials_exception
     return user
@@ -58,9 +66,8 @@ def get_db():
     finally:
         db.close()
 
-def authenticate_user(db: Session, email: str, password: str):
+def authenticate_user(email: str, password: str, db: Session):
     user = crud.authenticate_user(db, email, password)
-    if not user:
-        return False
+    if not user or not crud.verify_password(password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Неправильна електронна адреса чи пароль")
     return user
-
